70939d4a6780251acf4115a3f06c662b453d8ef9
E-Liang Tan <eliang@eliangtan.com>
2020-06-07 22:38:33 +0800

    Complete gentgen commands

--- src/cli/gentgen.ts
+++ src/cli/gentgen.ts
@@ -2,7 +2,7 @@
 
 import { Command } from "commander";
 
-import { getAllSchemaCodegenInfo } from "./getAllSchemaCodegenInfo";
+import { discoverSchemas } from "./discoverSchemas";
 import { ModelFileGenerator } from "../codegen/files/ModelFileGenerator";
 import { LoaderFileGenerator } from "../codegen/files/LoaderFileGenerator";
 import { QueryFileGenerator } from "../codegen/files/QueryFileGenerator";
@@ -14,30 +14,30 @@ program
   .command("discover [folder or schema path(s)]")
   .description("Discover schemas")
   .action((_, cmdObj: Command) => {
-    console.log("TODO: Discover in folders", cmdObj.args);
-    // TODO:
-    // const allCodegenInfo = getAllSchemaCodegenInfo(
-    //   cmdObj.args || [`${process.cwd()}/src/gents`]
-    // );
-    // allCodegenInfo.forEach((info) => console.log(info.filePath));
+    const allCodegenInfo = discoverSchemas(
+      cmdObj.args || [`${process.cwd()}/src/gents`]
+    );
+    allCodegenInfo.forEach((info) => console.log(info.filePath));
   });
 
 program
   .command("generate [folder or schema path(s)]")
   .description("Generate Gent classes from schemas")
+  .option("-v, --verbose")
   .action((_, cmdObj: Command) => {
-    console.log("TODO: Generate in folders", cmdObj.args);
-    // TODO:
-    // const allCodegenInfo = getAllSchemaCodegenInfo(
-    //   cmdObj.args || ["./src/gents"]
-    // );
-    // allCodegenInfo.forEach((info) => {
-    //   new ModelFileGenerator(codegenInfo).generate();
-    //   new LoaderFileGenerator(codegenInfo).generate();
-    //   new QueryFileGenerator(codegenInfo).generate();
-    //   new MutatorFileGenerator(codegenInfo).generate();
-    //   // TODO: GraphQlGraphEntityGenerator
-    // });
+    const allCodegenInfo = discoverSchemas(
+      cmdObj.args || [`${process.cwd()}/src/gents`]
+    );
+    allCodegenInfo.forEach((info) => {
+      if (cmdObj.verbose) {
+        console.log("Generating classes for", info.filePath);
+      }
+      new ModelFileGenerator(info).generate();
+      new LoaderFileGenerator(info).generate();
+      new QueryFileGenerator(info).generate();
+      new MutatorFileGenerator(info).generate();
+      // TODO: GraphQlGraphEntityGenerator
+    });
   });
 
 program.parse(process.argv);
