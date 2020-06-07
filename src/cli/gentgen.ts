97e8718d5273444f0dacb69b953cdd2dae0b1d3f
E-Liang Tan <eliang@eliangtan.com>
2020-06-07 22:47:49 +0800

    Add example commands

--- src/cli/gentgen.ts
+++ src/cli/gentgen.ts
@@ -10,18 +10,37 @@ import { MutatorFileGenerator } from "../codegen/files/MutatorFileGenerator";
 
 const program = new Command();
 
+program.description("Gent's generator CLI.").on("--help", function () {
+  console.log("");
+  console.log("Examples:");
+  console.log("");
+  console.log("  $ gentgen discover");
+  console.log("  $ gentgen generate");
+  console.log("  $ gentgen generate src/gents");
+  console.log("  $ gentgen generate src/gents/User/UserSchema.ts");
+});
+
 program
-  .command("discover [folder or schema path(s)]")
+  .command("discover [folder-or-schema-path(s)]")
   .description("Discover schemas")
   .action((_, cmdObj: Command) => {
     const allCodegenInfo = discoverSchemas(
       cmdObj.args || [`${process.cwd()}/src/gents`]
     );
     allCodegenInfo.forEach((info) => console.log(info.filePath));
+  })
+  .on("--help", function () {
+    console.log("");
+    console.log("Examples:");
+    console.log("");
+    console.log("  $ gentgen discover");
+    console.log("  $ gentgen discover src/gents");
+    console.log("  $ gentgen discover src/gents src/other-gents");
+    console.log("  $ gentgen discover src/gents/User/UserSchema.ts");
   });
 
 program
-  .command("generate [folder or schema path(s)]")
+  .command("generate [folder-or-schema-path(s)]")
   .description("Generate Gent classes from schemas")
   .option("-v, --verbose")
   .action((_, cmdObj: Command) => {
@@ -38,6 +57,15 @@ program
       new MutatorFileGenerator(info).generate();
       // TODO: GraphQlGraphEntityGenerator
     });
+  })
+  .on("--help", function () {
+    console.log("");
+    console.log("Examples:");
+    console.log("");
+    console.log("  $ gentgen generate");
+    console.log("  $ gentgen generate src/gents");
+    console.log("  $ gentgen generate src/gents/User/UserSchema.ts");
+    console.log("  $ gentgen generate src/gents src/gents/User/UserSchema.ts");
   });
 
 program.parse(process.argv);
