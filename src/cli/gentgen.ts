0713139b813efb9a46581014f431e76f14393a4a
E-Liang Tan <eliang@eliangtan.com>
2020-06-07 18:22:19 +0800

    Implement gentgen command processing with Commander.js

--- src/cli/gentgen.ts
+++ src/cli/gentgen.ts
@@ -1,20 +1,43 @@
 #!/usr/bin/env ts-node-script
 
+import { Command } from "commander";
+
 import { getAllSchemaCodegenInfo } from "./getAllSchemaCodegenInfo";
 import { ModelFileGenerator } from "../codegen/files/ModelFileGenerator";
 import { LoaderFileGenerator } from "../codegen/files/LoaderFileGenerator";
 import { QueryFileGenerator } from "../codegen/files/QueryFileGenerator";
 import { MutatorFileGenerator } from "../codegen/files/MutatorFileGenerator";
 
-function main() {
-  const allCodegenInfo = getAllSchemaCodegenInfo();
-  for (const codegenInfo of allCodegenInfo) {
-    new ModelFileGenerator(codegenInfo).generate();
-    new LoaderFileGenerator(codegenInfo).generate();
-    new QueryFileGenerator(codegenInfo).generate();
-    new MutatorFileGenerator(codegenInfo).generate();
-    // TODO: GraphQlGraphEntityGenerator
-  }
-}
+const program = new Command();
 
-main();
+program
+  .command("discover [folder or schema path(s)]")
+  .description("Discover schemas")
+  .action((_, cmdObj: Command) => {
+    console.log("TODO: Discover in folders", cmdObj.args);
+    // TODO:
+    // const allCodegenInfo = getAllSchemaCodegenInfo(
+    //   cmdObj.args || [`${process.cwd()}/src/gents`]
+    // );
+    // allCodegenInfo.forEach((info) => console.log(info.filePath));
+  });
+
+program
+  .command("generate [folder or schema path(s)]")
+  .description("Generate Gent classes from schemas")
+  .action((_, cmdObj: Command) => {
+    console.log("TODO: Generate in folders", cmdObj.args);
+    // TODO:
+    // const allCodegenInfo = getAllSchemaCodegenInfo(
+    //   cmdObj.args || ["./src/gents"]
+    // );
+    // allCodegenInfo.forEach((info) => {
+    //   new ModelFileGenerator(codegenInfo).generate();
+    //   new LoaderFileGenerator(codegenInfo).generate();
+    //   new QueryFileGenerator(codegenInfo).generate();
+    //   new MutatorFileGenerator(codegenInfo).generate();
+    //   // TODO: GraphQlGraphEntityGenerator
+    // });
+  });
+
+program.parse(process.argv);
