573488ceed731b3541d7760507ce1e227204e921
E-Liang Tan <eliang@eliangtan.com>
2020-06-13 23:25:02 +0800

    Register Comment model, fix MikroORM configs

--- example/src/gents/models.ts
+++ example/src/gents/models.ts
@@ -1,1 +1,2 @@
+export * from "./Comment/Comment";
 export * from "./Post/Post";
--- example/src/mikro-orm.config.ts
+++ example/src/mikro-orm.config.ts
@@ -1,18 +1,11 @@
 import { Options } from "mikro-orm";
-// import { Post } from "./gents/Post/Post";
 import * as Models from "./gents/models";
 
 const options: Options = {
   debug: true,
   dbName: "./temp/gentexample.sqlite",
   type: "sqlite",
-  discovery: {
-    // FIXME: Figure out why MikroORM throws `ValidationError` "Only abstract
-    // entities were discovered" when all entities are not abstract.
-    warnWhenNoEntities: true,
-  },
   entities: Object.values(Models),
-  // entities: [Post],
 };
 
 export default options;
