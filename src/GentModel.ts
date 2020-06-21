e93c0a4153e00dc9378c9a68c676ea2e8b34dd58
E-Liang Tan <eliang@eliangtan.com>
2020-06-21 15:36:21 +0800

    Fix GentModelClass to allow custom constructors

--- src/GentModel.ts
+++ src/GentModel.ts
@@ -1,14 +1,13 @@
 import { Primary } from "mikro-orm";
-import { CollectionItem } from "mikro-orm/dist/typings";
+import { CollectionItem, EntityClass } from "mikro-orm/dist/typings";
 
 export interface GentModel {
   id: number;
 }
 
-export interface GentModelClass<Model extends GentModel> extends Function {
-  new (): Model;
-}
+export type GentModelClass<Model extends GentModel> = EntityClass<Model>;
 
+// TODO: Restrict this to only Model's fields and relation IDs
 export type GentModelData<Model extends GentModel> = {
   [K in keyof Model]?:
     | Model[K]
